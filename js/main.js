e50a3aa215e0f673640a31b634286a82bfc58010
Marcos Fidelibus <fidelibusmarcos@gmail.com>
2023-10-15 21:18:07 -0300

    agregando acumulador de nombre de servicios

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ function saludar(){
 }
 
 const totalServiciosRedes = function() {
-    var servicioRedes = prompt(' Ingrese A si desea contratar el Plan A por 55usd,\n Ingrese B si desea contratar el Plan B por 65usd,\n Ingrese C si desea salir u obtener el total a pagar');
+    var servicioRedes = prompt(' Ingrese A si desea contratar el Plan A por 55usd,\n Ingrese B si desea contratar el Plan B por 65usd,\n Ingrese C si desea salir u obtener el total a pagar').toUpperCase();
     if (servicioRedes === 'A') {
         alert('Has seleccionado el Plan A por un valor de 55usd y se ha añadido a tu carrito');
+        stringServicios += ' Plan A(Redes) - 55usd;';
         return 55;
     } else if (servicioRedes === 'B') {
         alert('Has seleccionado el Plan B por un valor de 65usd y se ha añadido a tu carrito');
+        stringServicios += ' Plan B(Redes) - 65usd;';
         return 65;
     } else {
         return 0;
@@ -18,15 +20,18 @@ const totalServiciosRedes = function() {
 }
 
 const totalServiciosSesion = function() {
-    var servicioSesion = prompt(' Ingrese A si desea contratar la Sesión A por un valor de 25usd,\n Ingrese B si desea contratar la Sesión B por un valor de 30usd,\n Ingrese C si desea contratar la Sesión C por un valor de 44usd,\n Ingrese D si desea salir u obtener el total a pagar');
+    var servicioSesion = prompt(' Ingrese A si desea contratar la Sesión A por un valor de 25usd,\n Ingrese B si desea contratar la Sesión B por un valor de 30usd,\n Ingrese C si desea contratar la Sesión C por un valor de 44usd,\n Ingrese D si desea salir u obtener el total a pagar').toUpperCase();
     if (servicioSesion === 'A') {
         alert('Has seleccionado la Sesión A por un valor de 25usd y se ha añadido a tu carrito');
+        stringServicios += ' Plan A(Sesion) - 25usd;';
         return 25;
     } else if (servicioSesion === 'B') {
         alert('Has seleccionado la Sesión B por un valor de 30usd y se ha añadido a tu carrito');
+        stringServicios += ' Plan B(Sesion) - 30usd;';
         return 30;
     } else if (servicioSesion === 'C') {
         alert('Has seleccionado la Sesión C por un valor de 44usd y se ha añadido a tu carrito');
+        stringServicios += ' Plan C(Sesion) - 44usd;';
         return 44;
     } else {
         return 0;
@@ -34,22 +39,20 @@ const totalServiciosSesion = function() {
 }
 
 function seleccionarServicios(totalServiciosRedes,totalServiciosSesion) {
-    while (true) {
+    while (opcion !== '3') {
         var opcion = prompt('Ingrese 1 si desea adquirir un servicio de manejo de redes,\nIngrese 2 si desea adquirir un servicio sesión de contenido,\nIngrese 3 si desea salir u obtener el total a pagar');
     
         if (opcion === '1') {
             total += totalServiciosRedes();
         } else if (opcion === '2') {
             total += totalServiciosSesion();
-        } else if (opcion === '3') {
-            break;
         }
     }
 }
 
-function informarTotal(valor) {
+function informarTotal(servicios,valor) {
     if (valor > 0) {
-        alert('Total a pagar: $' + total);
+        alert('El total a pagar por los servicios: '+servicios+ ' es de $' + total);
     } else {
         alert('No se seleccionaron servicios.');
     }
@@ -58,6 +61,7 @@ function informarTotal(valor) {
 // VARIABLES
 
 var total = 0;
+var stringServicios = '';
 
 // INICIO DEL CODIGO
 
@@ -67,4 +71,4 @@ saludar();
 seleccionarServicios(totalServiciosRedes,totalServiciosSesion);
 
 
-informarTotal(total);
+informarTotal(stringServicios,total);
