9e7da2dc012272253d9df6c3983d0cb6c5e45b82
Marcos Fidelibus <fidelibusmarcos@gmail.com>
2023-10-27 22:22:08 -0300

    Segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,74 +1,137 @@
 // FUNCIONES
 
-function saludar(){
+function saludar() {
     alert('Hola! Bienvenido al sector servicios. Aquí podrás seleccionar los servicios que deseas contratar');
 }
 
-const totalServiciosRedes = function() {
-    var servicioRedes = prompt(' Ingrese A si desea contratar el Plan A por 55usd,\n Ingrese B si desea contratar el Plan B por 65usd,\n Ingrese C si desea salir u obtener el total a pagar').toUpperCase();
-    if (servicioRedes === 'A') {
-        alert('Has seleccionado el Plan A por un valor de 55usd y se ha añadido a tu carrito');
-        stringServicios += ' Plan A(Redes) - 55usd;';
-        return 55;
-    } else if (servicioRedes === 'B') {
-        alert('Has seleccionado el Plan B por un valor de 65usd y se ha añadido a tu carrito');
-        stringServicios += ' Plan B(Redes) - 65usd;';
-        return 65;
-    } else {
-        return 0;
-    }
+function existeServicioEnCarrito(nombreServicio) {
+    return carrito.some(item => item.nombre === nombreServicio);
 }
 
-const totalServiciosSesion = function() {
-    var servicioSesion = prompt(' Ingrese A si desea contratar la Sesión A por un valor de 25usd,\n Ingrese B si desea contratar la Sesión B por un valor de 30usd,\n Ingrese C si desea contratar la Sesión C por un valor de 44usd,\n Ingrese D si desea salir u obtener el total a pagar').toUpperCase();
-    if (servicioSesion === 'A') {
-        alert('Has seleccionado la Sesión A por un valor de 25usd y se ha añadido a tu carrito');
-        stringServicios += ' Plan A(Sesion) - 25usd;';
-        return 25;
-    } else if (servicioSesion === 'B') {
-        alert('Has seleccionado la Sesión B por un valor de 30usd y se ha añadido a tu carrito');
-        stringServicios += ' Plan B(Sesion) - 30usd;';
-        return 30;
-    } else if (servicioSesion === 'C') {
-        alert('Has seleccionado la Sesión C por un valor de 44usd y se ha añadido a tu carrito');
-        stringServicios += ' Plan C(Sesion) - 44usd;';
-        return 44;
-    } else {
-        return 0;
-    }
-}
-
-function seleccionarServicios(totalServiciosRedes,totalServiciosSesion) {
-    while (opcion !== '3') {
-        var opcion = prompt('Ingrese 1 si desea adquirir un servicio de manejo de redes,\nIngrese 2 si desea adquirir un servicio sesión de contenido,\nIngrese 3 si desea salir u obtener el total a pagar');
-    
-        if (opcion === '1') {
-            total += totalServiciosRedes();
-        } else if (opcion === '2') {
-            total += totalServiciosSesion();
+const agregarServiciosRedes = function() {
+    var servicioRedes;
+    while (servicioRedes !== 'C') {
+        servicioRedes = prompt(`Ingrese A si desea contratar el ${listaDeServicios[0].nombre} por $${listaDeServicios[0].precio},\nIngrese B si desea contratar el ${listaDeServicios[1].nombre} por $${listaDeServicios[1].precio},\nIngrese C si desea salir u obtener el total a pagar`).toUpperCase();
+        switch (servicioRedes) {
+            case "A":
+                if (!existeServicioEnCarrito(listaDeServicios[0].nombre)) {
+                    alert(`Has seleccionado el ${listaDeServicios[0].nombre} por un valor de $${listaDeServicios[0].precio} y se ha añadido a tu carrito`);
+                    carrito.push(listaDeServicios[0]);
+                } else {
+                    alert(`El servicio ${listaDeServicios[0].nombre} ya está en el carrito.`);
+                }
+                break;
+            case "B":
+                if (!existeServicioEnCarrito(listaDeServicios[1].nombre)) {
+                    alert(`Has seleccionado el ${listaDeServicios[1].nombre} por un valor de $${listaDeServicios[1].precio} y se ha añadido a tu carrito`);
+                    carrito.push(listaDeServicios[1]);
+                } else {
+                    alert(`El servicio ${listaDeServicios[1].nombre} ya está en el carrito.`);
+                }
+                break;
+            case "C":
+                break;
+            default:
+                alert('OPCIÓN INCORRECTA');
         }
     }
 }
 
-function informarTotal(servicios,valor) {
-    if (valor > 0) {
-        alert('El total a pagar por los servicios: '+servicios+ ' es de $' + total);
+const agregarServiciosSesion = function() {
+    var servicioSesion;
+    while (servicioSesion !== 'D') {
+        servicioSesion = prompt(`Ingrese A si desea contratar la ${listaDeServicios[2].nombre} por $${listaDeServicios[2].precio},\nIngrese B si desea contratar la ${listaDeServicios[3].nombre} por $${listaDeServicios[3].precio},\nIngrese C si desea contratar la ${listaDeServicios[4].nombre} por $${listaDeServicios[4].precio},\nIngrese D si desea salir u obtener el total a pagar`).toUpperCase();
+        switch (servicioSesion) {
+            case "A":
+                if (!existeServicioEnCarrito(listaDeServicios[2].nombre)) {
+                    alert(`Has seleccionado la ${listaDeServicios[2].nombre} por un valor de $${listaDeServicios[2].precio} y se ha añadido a tu carrito`);
+                    carrito.push(listaDeServicios[2]);
+                } else {
+                    alert(`El servicio ${listaDeServicios[2].nombre} ya está en el carrito.`);
+                }
+                break;
+            case "B":
+                if (!existeServicioEnCarrito(listaDeServicios[3].nombre)) {
+                    alert(`Has seleccionado la ${listaDeServicios[3].nombre} por un valor de $${listaDeServicios[3].precio} y se ha añadido a tu carrito`);
+                    carrito.push(listaDeServicios[3]);
+                } else {
+                    alert(`El servicio ${listaDeServicios[3].nombre} ya está en el carrito.`);
+                }
+                break;
+            case "C":
+                if (!existeServicioEnCarrito(listaDeServicios[4].nombre)) {
+                    alert(`Has seleccionado la ${listaDeServicios[4].nombre} por un valor de $${listaDeServicios[4].precio} y se ha añadido a tu carrito`);
+                    carrito.push(listaDeServicios[4]);
+                } else {
+                    alert(`El servicio ${listaDeServicios[4].nombre} ya está en el carrito.`);
+                }
+                break;
+            case "D":
+                break;
+            default:
+                alert('OPCIÓN INCORRECTA');
+        }
+    }
+}
+
+function seleccionarServicios() {
+    var opcion;
+    while (opcion !== '3') {
+        opcion = prompt('Ingrese 1 si desea adquirir un servicio de manejo de redes,\nIngrese 2 si desea adquirir un servicio sesión de contenido,\nIngrese 3 si desea salir u obtener el total a pagar');
+        switch (opcion) {
+            case "1":
+                agregarServiciosRedes();
+                break;
+
+            case "2":
+                agregarServiciosSesion();
+                break;
+
+            case "3":
+                alert('Has finalizado tu operación');
+                break;
+
+            default:
+                alert('OPCIÓN INCORRECTA');
+                break;
+        }
+    }
+}
+
+function informarTotal(servicios) {
+    let total = servicios.reduce((accumulator, item) => accumulator + item.precio, 0);
+    if (total > 0) {
+        let serviciosSeleccionados = servicios.map(item => `\n- ${item.nombre} - $${item.precio}`).join(', ');
+        alert(`Servicios seleccionados en el carrito: ${serviciosSeleccionados}\nEl total a pagar es de $${total}`);
     } else {
         alert('No se seleccionaron servicios.');
     }
 }
 
+
+// Objetos
+
+class Servicio {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+const listaDeServicios = [
+    new Servicio('Plan A (Redes)', 55),
+    new Servicio('Plan B (Redes)', 65),
+    new Servicio('Sesión A', 25),
+    new Servicio('Sesión B', 30),
+    new Servicio('Sesión C', 44),
+];
+
 // VARIABLES
 
-var total = 0;
-var stringServicios = '';
+const carrito = [];
 
-// INICIO DEL CODIGO
+// INICIO DEL CÓDIGO
 
 saludar();
-
-
-seleccionarServicios(totalServiciosRedes,totalServiciosSesion);
-
-
-informarTotal(stringServicios,total);
+seleccionarServicios();
+informarTotal(carrito);
